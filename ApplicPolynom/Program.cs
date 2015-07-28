0f49200acda676c3124026c8237d8f00b8aa3382
Viktoryia Lazouskaya <Viktoryia_Lazouskaya@epam.com>
2015-07-28 13:04:36 +0300

    Add task4

---
+++ ApplicPolynom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polynomial;
+
+namespace ApplicPolynom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynom a=new Polynom(1,2,3,4,5);
+            Polynom b = new Polynom(1, 2, 3);
+            Polynom c = a + b;
+            foreach (double koeff in c.Koeff)
+            {
+                Console.WriteLine(koeff);
+            }
+            Console.WriteLine();
+            c = a - b;
+            foreach (double koeff in c.Koeff)
+            {
+                Console.WriteLine(koeff);
+            }
+            Console.ReadKey();
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Polynomial/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynomial
+{
+    public class Polynom
+    {
+        public double[] Koeff { get; set; }
+        private int Degree { get; set; }
+        public Polynom(params double[] koefficients)
+        {
+            Koeff= koefficients;
+            Degree = koefficients.Length-1;
+        }
+
+        public static Polynom operator +(Polynom a, Polynom b)
+        {
+            Polynom greater;
+            Polynom less;
+            if (a.Degree < b.Degree){
+                less = a;
+                greater = b;}
+            else{
+                greater = a;
+                less = b; }
+            double[] newKoeff = new double[greater.Degree+1];
+            for (int i=0; i<=greater.Degree; i++)
+            { newKoeff[i] = greater.Koeff[i];}
+            for (int i = 0; i <= less.Degree; i++)
+            { newKoeff[i] += less.Koeff[i];}
+            Polynom sum = new Polynom(newKoeff);
+            return sum;
+        }
+
+        public static Polynom operator -(Polynom a, Polynom b)
+        {
+            Polynom greater;
+            Polynom less;
+            if (a.Degree < b.Degree)
+            {
+                less = a;
+                greater = b;
+            }
+            else
+            {
+                greater = a;
+                less = b;
+            }
+            double[] newKoeff = new double[greater.Degree + 1];
+            for (int i = 0; i <= greater.Degree; i++)
+            { newKoeff[i] = greater.Koeff[i]; }
+            for (int i = 0; i <= less.Degree; i++)
+            { newKoeff[i] -= less.Koeff[i]; }
+            //следует добавить удаление нулей из конца массива;
+            Polynom minus = new Polynom(newKoeff);
+            return minus;
+        }
+    }
+}
